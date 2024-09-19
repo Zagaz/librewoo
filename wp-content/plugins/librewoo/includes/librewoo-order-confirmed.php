072d80465b18afc197e3d997c81e79c3f466b863
Zagaz <andre_ranulfo@hotmail.com>
2024-09-19 17:08:11 -0300

    Refactor order completion message and logging, improve code structure and remove commented code

--- wp-content/plugins/librewoo/includes/librewoo-order-confirmed.php
+++ wp-content/plugins/librewoo/includes/librewoo-order-confirmed.php
@@ -1,77 +1,63 @@
-<?php 
+<?php
 // You shall not pass!
 if (!defined('ABSPATH')) {
     exit;
 }
 
+/**
+ * Class WooOrderComplete
+ * 
+ * 
+ * @since 1.0.0
+ */
 
-class WooOrderComplete {
-    public function __construct() {
-        // Hook into WooCommerce order status change to "processing"
+class WooOrderComplete
+{
+    public function __construct()
+    {
         add_action('woocommerce_order_status_processing', [$this, 'order_complete_message']);
     }
 
-    public function order_complete_message($order_id) {
+    public function order_complete_message($order_id)
+    {
 
         // Ensure the order ID is valid     
         $order_id = absint($order_id);
-       
+
         if (!$order_id) {
             return;
         }
-        if (!is_numeric($order_id)){
+        if (!is_numeric($order_id)) {
             return;
         }
 
-        $order = wc_get_order($order_id);
-        $woo_client_info = $this->get_order_data($order, $order_id);
 
-        // Log the order details, including products purchased
-        // $log_message = $this->create_log_message($woo_client_info);
+        /**
+         * Get order data from WooCommerce order object
+         * Data: customer name, customer email, purchased items
+         * 
+         */
+        $this->get_order_data(wc_get_order($order_id));
         
-        // Trigger LibreSign pipeline
-        //$this->librewoo_trigger($log_message);
-        
-        // Log the message
-        //$this->error_logger($log_message);
-
-       
-        $this->librewoo_trigger($woo_client_info);
-
-        // get get_order_data() and store in a variable.
-
-        $order_id = $order->get_id();
-
-        $test = $this->get_order_data($order_id);
-        $this->librewoo_trigger($test);
-
-
+        $this->librewoo_trigger($this->get_order_data(wc_get_order($order_id)));
     }
 
-    private function get_order_data($order_id) {
-        $order= $order_id;
+/**
+ * Get order data from WooCommerce order object
+ * Data: customer name, customer email, purchased items
+ * 
+ * @param WC_Order $order
+ * @return stdClass
+ * @since 1.0.0
+ */ 
+    private function get_order_data($order_id)
+    {
+        $order = $order_id;
+        
         $woo_client_info = new stdClass();
-        //Id do pedido
-        // $woo_client_info->order_id = $order_id;
-        // Informações do cliente - Primeiro nome
         $woo_client_info->customer_name = $order->get_billing_first_name();
-        // Informações do cliente - Sobrenome
         $woo_client_info->customer_last_name = $order->get_billing_last_name();
-        // Informações do cliente - E-mail
         $woo_client_info->customer_email = $order->get_billing_email();
-        // Informações do cliente - Telefone
-        // $woo_client_info->customer_phone = $order->get_billing_phone();
-        // Informações do cliente - Endereço
-        // $woo_client_info->payment_method = $order->get_payment_method_title(); // e.g., 'PayPal'
-        // Informações do cliente - Data de pagamento
-        // $payment_date = $order->get_date_paid();
-        // Data do pagamento formatada
-        // $woo_client_info->payment_date = $payment_date ? $payment_date->date('F j, Y @ h:i a') : 'Not paid yet'; // Format the date
-        // Informações do cliente - ID da transação
-        // $woo_client_info->transaction_id = $order->get_transaction_id(); // PayPal transaction ID
-        // Informações do cliente - IP
-        // $woo_client_info->customer_ip = $order->get_customer_ip_address(); // Customer IP
-
         // Retrieve purchased items
         $woo_client_info->purchased_items = [];
         /// Loop through each order item
@@ -87,45 +73,24 @@ class WooOrderComplete {
         return $woo_client_info;
     }
 
-    // private function create_log_message($woo_client_info) {
-    //     $log_message = 'Payment via ' . $woo_client_info->payment_method . 
-    //         ' was completed on ' . $woo_client_info->payment_date . 
-    //         ' for ' . $woo_client_info->customer_name . 
-    //         ' ' . $woo_client_info->customer_last_name . 
-    //         ' with transaction ID ' . $woo_client_info->transaction_id . 
-    //         ' from IP ' . $woo_client_info->customer_ip . "\n";
-
-    //     $log_message .= "Purchased products:\n";
-        
-    //     foreach ($woo_client_info->purchased_items as $item) {
-    //         $log_message .= '- ' . $item['name'] . ': ' . $item['quantity'] . ' x ' . $item['total'] . "\n";
-    //     }
-
-    //     return $log_message;
-    // }
-
-   
-        
-    private function librewoo_trigger($order_data) {
-
-        
-        $array = get_object_vars($order_data);
-        $email = $array['customer_email'];
-        $name = $array['customer_name'] . ' '. $array['customer_last_name'];         
-        $quota = $array['purchased_items'][0]['name'];
-
-        echo "Email: $email <br>";
-        echo "Nome: $name <br>";
-        echo "Quota: $quota <br>";
-        
-
-     
-    }
 
 
+
+/**
+ *
+ * Trigger LibreSign API
+ * @return void
+ * @since 1.0.0
+ */
+    private function librewoo_trigger($order_data)
+    {
+
+
+        $order_data = get_object_vars($order_data);
+
+        // $email = $order_data['customer_email'];
+        // $name = $order_data['customer_name'] . ' '. $order_data['customer_last_name'];         
+        // $quota = $order_data['purchased_items'][0]['name'];
 
     }
-
-   
-
-
+}
