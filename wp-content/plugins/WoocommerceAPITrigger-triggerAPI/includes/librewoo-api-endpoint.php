0092d4b89d82617195a8db03b3713efe939b0a89
Zagaz <andre_ranulfo@hotmail.com>
2024-10-08 15:06:07 -0300

    Refactor order completion message and logging, validate email, name, and quota on LibreSignEndpoint class

--- wp-content/plugins/WoocommerceAPITrigger-triggerAPI/includes/librewoo-api-endpoint.php
+++ wp-content/plugins/WoocommerceAPITrigger-triggerAPI/includes/librewoo-api-endpoint.php
@@ -18,69 +18,65 @@ class LibreSignEndpoint
         $this->authorization = $authorization;
         // call logData method
         $this->logData();
+        // call triggerAPI method'
+        $this->triggerAPI();
 
        
 
     }
 
-
-public function logData(){
-        // logo all data to a file
-        $log = "API call data: \n";
-        $log = "groupid: " . $this->groupid . "\n";
-        $log .= "display_name: " . $this->display_name . "\n";
-        $log .= "quota: " . $this->quota . "\n";
-        $log .= "apps: " . $this->apps . "\n";
-        $log .= "authorization: " .
-        $this->authorization . "\n";
+     private function triggerAPI() {
+        $url = 'http://localhost/ocs/v2.php/apps/admin_group_manager/api/v1/admin-group';
         
-        error_log($log, 3, "libresign.log");
+        $body = [
+            'groupid'     => $this->groupid,
+            'displayname' => $this->display_name,
+            'quota'       => $this->quota,
+            'apps'        => $this->apps
+        ];
+
+        $headers = [
+            'Accept'        => 'application/json',
+            'Authorization' => 'Basic ' . base64_encode($this->authorization),
+            'Content-Type'  => 'application/json',
+            'OCS-APIRequest' => 'true',
+        ];
+
+        $response = wp_remote_post($url, [
+            'body'    => json_encode($body),
+            'headers' => $headers,
+        ]);
+
+        if (is_wp_error($response)) {
+            return 'Erro: ' . $response->get_error_message();
+        } else{
+            // error_log the response
+            error_log(wp_remote_retrieve_body($response));
+        }
+        
+      
+
+
+        return wp_remote_retrieve_body($response);
 
     }
-    // class LibreSignEndpoint {
-    //     private $groupid;
-    //     private $display_name;
-    //     private $quota;
-    //     private $apps;
-    //     private $authorization;
-    
-    //     public function __construct($groupid, $display_name, $quota, $apps, $authorization) {
-    //         $this->groupid = $groupid;
-    //         $this->display_name = $display_name;
-    //         $this->quota = $quota;
-    //         $this->apps = $apps;
-    //         $this->authorization = $authorization;
-    //     }
-    
-    //     public function triggerAPI() {
-    //         $url = 'http://localhost/ocs/v2.php/apps/admin_group_manager/api/v1/admin-group';
-            
-    //         $body = [
-    //             'groupid'     => $this->groupid,
-    //             'displayname' => $this->display_name,
-    //             'quota'       => $this->quota,
-    //             'apps'        => $this->apps
-    //         ];
-    
-    //         $headers = [
-    //             'Accept'        => 'application/json',
-    //             'Authorization' => 'Basic ' . base64_encode($this->authorization),
-    //             'Content-Type'  => 'application/json',
-    //             'OCS-APIRequest' => 'true',
-    //         ];
-    
-    //         $response = wp_remote_post($url, [
-    //             'body'    => json_encode($body),
-    //             'headers' => $headers,
-    //         ]);
-    
-    //         if (is_wp_error($response)) {
-    //             return 'Erro: ' . $response->get_error_message();
-    //         }
-    
-    //         return wp_remote_retrieve_body($response);
-    //     }
-    // }
-   
+
+
+
+    private function logData()
+    {
+        error_log(
+            sprintf(
+                "||| LibreSign: Name: %s Email: %s Quota: %s",
+                $this->display_name,
+                $this->groupid,
+                $this->quota
+            )
+        );
+    }
+
 }
+
+
+
 
