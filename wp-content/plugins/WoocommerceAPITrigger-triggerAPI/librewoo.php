0837b6142c8be316e5533b83b889f9f4bf3721c6
Zagaz <andre_ranulfo@hotmail.com>
2024-10-09 15:20:35 -0300

    Refactor logging and add new log files for LibreSign API response and order confirmation

--- wp-content/plugins/WoocommerceAPITrigger-triggerAPI/librewoo.php
+++ wp-content/plugins/WoocommerceAPITrigger-triggerAPI/librewoo.php
@@ -21,6 +21,17 @@ new WooOneProductCart();
 new WooOrderComplete();
 
 
+add_action('woocommerce_subscription_status_active', 'log_subscription_id_on_pending', 10, 1);
+
+function log_subscription_id_on_pending($subscription) {
+    // Obter ID da assinatura
+    $subscription_id = $subscription->get_id();
+
+    // Registrar o ID da assinatura no log
+    error_log('Assinatura ' . $subscription_id . ' está agora pendente.');
+}
+
+
 
 
 
