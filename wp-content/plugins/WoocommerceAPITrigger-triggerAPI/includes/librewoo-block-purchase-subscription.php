7f9982d5b42ab7e10ef5f41a08269ece53f4a8f1
Zagaz <andre_ranulfo@hotmail.com>
2024-10-18 16:33:54 -0300

    Formating to WP Standards

--- wp-content/plugins/WoocommerceAPITrigger-triggerAPI/includes/librewoo-block-purchase-subscription.php
+++ wp-content/plugins/WoocommerceAPITrigger-triggerAPI/includes/librewoo-block-purchase-subscription.php
@@ -1,7 +1,7 @@
 <?php
 // You shall not pass!
-if (! defined('ABSPATH')) {
-  exit;
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
 }
 
 /**
@@ -13,58 +13,47 @@ if (! defined('ABSPATH')) {
  *
  * @since 1.0.0
  */
+class LibreSignBlockPurchaseSameSubscription {
 
-class LibreSignBlockPurchaseSameSubscription
-{
-
-  /**
-   * Constructor
-   */
-  
-
-  public function __construct()
-  {
-    // Add action when the user clicks on the add to cart button
-    add_action(
-      'woocommerce_add_to_cart',
-      array($this, 'block_purchase_same_subscription'),
-      10,
-      6
-    );
-  }
-
-  /**
-   * Block purchase if the user already has an active subscription for the same product.
-   */
-  public function block_purchase_same_subscription()
-  {
-    if (!is_user_logged_in()) {
-      return;
+    /**
+     * Constructor
+     */
+    public function __construct() {
+        // Add action when the user clicks on the add to cart button
+        add_action(
+            'woocommerce_add_to_cart',
+            array( $this, 'block_purchase_same_subscription' ),
+            10,
+            6
+        );
     }
 
-    //get user id
-    $user_id = get_current_user_id();
-    // get the cart items
-    $cart_items = WC()->cart->get_cart();
-    // get the product id of all items in the cart
-    $cart_product_id = (reset($cart_items)['product_id']);
-    
+    /**
+     * Block purchase if the user already has an active subscription for the same product.
+     */
+    public function block_purchase_same_subscription() {
+        if ( ! is_user_logged_in() ) {
+            return;
+        }
 
-    $is_subscription_active = new LibreSignSubscruptionStatusChecker();
-    $check_subs = $is_subscription_active->check_subscription_status($user_id ,  $cart_product_id );
-    
+        $user_id        = get_current_user_id();
+        $cart_items     = WC()->cart->get_cart();
+        $cart_product_id = reset( $cart_items )['product_id'];
 
-   // If any subscription is active for the same product, block the purchase.
-    for ($i = 0; $i < count($check_subs); $i++) {
-      if (
-        $cart_product_id == $check_subs[$i]['product_id'] &&
-        $check_subs[$i]['status'] == 'active'
-      ) {
-        wc_add_notice('You already subscribed this product and it\'s ' . $check_subs[$i]['status'].'.', 'error');
-        add_filter('wc_add_to_cart_message_html', '__return_empty_string', 10, 2);
-        WC()->cart->remove_cart_item(key($cart_items));
-      }
+        // Check if the user has any active subscription for the same product. 
+        $is_subscription_active = new LibreSignSubscruptionStatusChecker();
+        $check_subs = $is_subscription_active->check_subscription_status( $user_id, $cart_product_id );
+
+        // If any subscription is active for the same product, block the purchase.
+        for ( $i = 0; $i < count( $check_subs ); $i++ ) {
+            if ( $cart_product_id == $check_subs[ $i ]['product_id'] && $check_subs[ $i ]['status'] == 'active' ) {
+                wc_add_notice( 
+                    'You already subscribed to this product and it\'s ' . $check_subs[ $i ]['status'] . '.', 
+                    'error' 
+                );
+                add_filter( 'wc_add_to_cart_message_html', '__return_empty_string', 10, 2 );
+                WC()->cart->remove_cart_item( key( $cart_items ) );
+            }
+        }
     }
-
-  }
 }
--- wp-content/plugins/WoocommerceAPITrigger-triggerAPI/includes/librewoo-subscruption-status-checker.php
+++ wp-content/plugins/WoocommerceAPITrigger-triggerAPI/includes/librewoo-subscruption-status-checker.php
@@ -1,43 +1,40 @@
 <?php
 
-if (!defined('ABSPATH')) {
+if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-class LibreSignSubscruptionStatusChecker
-{
-    public function __construct()
-    {
+class LibreSignSubscruptionStatusChecker {
+
+    public function __construct() {
         $this->test();
     }
 
-    public function test()
-    {
-        return "test";
+    public function test() {
+        return 'test';
     }
 
-    public function check_subscription_status($user_id, $cart_product_id)
-    {
+    public function check_subscription_status( $user_id, $cart_product_id ) {
 
-        $subscriptions = wcs_get_users_subscriptions($user_id);
+        $subscriptions     = wcs_get_users_subscriptions( $user_id );
         $subscription_data = array();
 
-        // filter $subscriptions if any subscriptions is active.
-
-        foreach ($subscriptions as $sub) {
-            $order = wc_get_order($sub->get_parent_id());
+        // Filter $subscriptions if any subscription is active.
+        foreach ( $subscriptions as $sub ) {
+            $order = wc_get_order( $sub->get_parent_id() );
             $items = $order->get_items();
-            foreach ($items as $item) {
-                $data = $item->get_data();
+
+            foreach ( $items as $item ) {
+                $data             = $item->get_data();
                 $temp_item['item'] = $data['product_id'];
-        
             }
+
             $subscription_data[] = array(
-                'subscription_id' => $sub->get_id(),
-                'parent_id' => $sub->get_parent_id(),
-                'status'    => $sub->get_status(),
-                'product_id' => $temp_item['item'],
-                'subscription_date_expires' => $sub->get_date('end'),
+                'subscription_id'         => $sub->get_id(),
+                'parent_id'               => $sub->get_parent_id(),
+                'status'                  => $sub->get_status(),
+                'product_id'              => $temp_item['item'],
+                'subscription_date_expires' => $sub->get_date( 'end' ),
             );
         }
 
