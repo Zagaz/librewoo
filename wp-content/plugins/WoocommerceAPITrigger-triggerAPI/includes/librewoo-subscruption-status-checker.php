02e0dfd666e4f2349ee202c9e8aaf9142519a75f
Zagaz <andre_ranulfo@hotmail.com>
2024-10-18 15:36:41 -0300

    Fix the $subscription_data

--- wp-content/plugins/WoocommerceAPITrigger-triggerAPI/includes/librewoo-subscruption-status-checker.php
+++ wp-content/plugins/WoocommerceAPITrigger-triggerAPI/includes/librewoo-subscruption-status-checker.php
@@ -9,49 +9,54 @@ class LibreSignSubscruptionStatusChecker
     public function __construct()
     {
         $this->test();
-        
     }
 
-    public function test(){
+    public function test()
+    {
         return "test";
     }
 
     public function check_subscription_status($user_id, $cart_product_id)
     {
-        var_dump(json_encode($cart_product_id));
+
         $subscriptions = wcs_get_users_subscriptions($user_id);
         $subscription_data = array();
 
-          foreach ($subscriptions as $sub) {
-      $order = wc_get_order($sub->get_parent_id());
-      $items = $order->get_items();
-      foreach ($items as $item) {
-        $data = $item->get_data();
-        $subscription_data['product_id'] = $data['product_id'];
-      }
-      $subscription_data[] = array(
-        'subscription_id' => $sub->get_id(),
-        'parent_id' => $sub->get_parent_id(),
-        'status'    => $sub->get_status(),
-        'product_id' => $subscription_data['product_id'],
+        // filter $subscriptions if any subscriptions is active.
 
-      );
-    }
-    
+        foreach ($subscriptions as $sub) {
+            $order = wc_get_order($sub->get_parent_id());
+            $items = $order->get_items();
+            foreach ($items as $item) {
+                $data = $item->get_data();
+                $subscription_data['user_id'] =  $user_id;
+            }
+            $subscription_data[] = array(
+                'subscription_id' => $sub->get_id(),
+                'parent_id' => $sub->get_parent_id(),
+                'status'    => $sub->get_status(),
+                'product_id' => $subscription_data['user_id'],
 
-      
-
+            );
+        }
 
 
-        var_dump(json_encode($subscription_data));
-  
+         
+       
+        
 
-        return $subscription_data;
+        
 
 
+echo "<pre>";
+var_dump($subscription_data);
 
      
+
+        // var_dump(json_encode($subscription_data));
+        // echo "</pre>";
+
+
+        return $subscription_data;
     }
-
 }
-
