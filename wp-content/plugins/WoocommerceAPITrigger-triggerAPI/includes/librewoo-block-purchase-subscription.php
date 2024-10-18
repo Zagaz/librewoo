ac14ff227c546a7f49d8c9456e1785bc9af376cf
Zagaz <andre_ranulfo@hotmail.com>
2024-10-18 17:26:36 -0300

    Refactor class name to accurately reflect its purpose

--- wp-content/plugins/WoocommerceAPITrigger-triggerAPI/includes/librewoo-block-purchase-subscription.php
+++ wp-content/plugins/WoocommerceAPITrigger-triggerAPI/includes/librewoo-block-purchase-subscription.php
@@ -28,48 +28,11 @@ class LibreSignBlockPurchaseSameSubscriptionAndStatus
             6
         );
     }
-        /**
-     * Block purchase if the user already has an active subscription for the same product.
-     */
-    public function block_purchase_same_subscription_and_active() 
-    {
-      if ( ! is_user_logged_in() ) {
-          return;
-      }
-
-      $user_id        = get_current_user_id();
-      $cart_items     = WC()->cart->get_cart();
-      $cart_product_id = reset( $cart_items )['product_id'];
-
-      // Check if the user has any active subscription for the same product. 
-      $is_subscription_active = new LibreSignSubscruptionStatusChecker();
-      $check_subs = $is_subscription_active->check_subscription_status( $user_id, $cart_product_id );
-
-      // If any subscription is active for the same product, block the purchase.
-      for ( $i = 0; $i < count( $check_subs ); $i++ ) {
-          if ( $cart_product_id == $check_subs[ $i ]['product_id'] && $check_subs[ $i ]['status'] == 'active' ) {
-              wc_add_notice( 
-                  'You can\'t subscribe to this product because you already have it and it\'s <strong> 
-                  <span style="color: green; text-transform: Capitalize;">
-                  ' . $check_subs[ $i ]['status'] . '. 
-                  </span>
-                  </strong> Please consider an 
-                  <strong><a href="#">
-                  upgrade</a> </strong> 
-                  or 
-                  <strong><a href="#">contact support</a></strong> if you have any questions.', 
-                  'error' 
-              );
-              add_filter( 'wc_add_to_cart_message_html', '__return_empty_string', 10, 2 );
-              WC()->cart->remove_cart_item( key( $cart_items ) );
-          }
-      }
-  }
 
     /**
      * Block purchase if the user already has an active subscription for the same product.
      */
-    public function block_purchase_same_subscription_and_activeOlD() {
+    public function block_purchase_same_subscription_and_active() {
         if ( ! is_user_logged_in() ) {
             return;
         }
@@ -82,13 +45,36 @@ class LibreSignBlockPurchaseSameSubscriptionAndStatus
         $is_subscription_active = new LibreSignSubscruptionStatusChecker();
         $check_subs = $is_subscription_active->check_subscription_status( $user_id, $cart_product_id );
 
+        $message_active = 'You can\'t subscribe to this product because you already have it and it\'s <strong>
+        <span style="color: green; text-transform: Capitalize;">active</span></strong>. Please consider an
+        <strong><a href="#">upgrade</a> </strong> or <strong><a href="#">contact support</a></strong> if you have any questions.';
+        $message_on_hold = 'You can\'t subscribe to this product because you already have it and it\'s <strong>
+        <span style="color: orange; text-transform: Capitalize;">on hold</span></strong>. Please consider an
+        <strong><a href="#">upgrade</a> </strong> or <strong><a href="#">contact support</a></strong> if you have any questions.';
+        $message_expired = 'You can\'
+        t subscribe to this product because you already have it and it\'s <strong>
+        <span style="color: red; text-transform: Capitalize;">expired</span></strong>. Please consider an
+        <strong><a href="#">upgrade</a> </strong> or <strong><a href="#">contact support</a></strong> if you have any questions.';
+        $message_payment_failed = 'You can\'t subscribe to this product because you already have it and it\'s <strong>
+        <span style="color: red; text-transform: Capitalize;">payment failed</span></strong>. Please consider an
+        <strong><a href="#">upgrade</a> </strong> or <strong><a href="#">contact support</a></strong> if you have any questions.';
+        $message_cancelled = 'You can\'t subscribe to this product because you already have it and it\'s <strong>
+        <span style="color: red; text-transform: Capitalize;">cancelled</span></strong>. Please consider an
+        <strong><a href="#">upgrade</a> </strong> or <strong><a href="#">contact support</a></strong> if you have any questions.';
+        $message_suspended = 'You can\'t subscribe to this product because you already have it and it\'s <strong>
+        <span style="color: red; text-transform: Capitalize;">suspended</span></strong>. Please consider an
+        <strong><a href="#">upgrade</a> </strong> or <strong><a href="#">contact support</a></strong> if you have any questions.';
+        
+        
         // If any subscription is active for the same product, block the purchase.
         for ( $i = 0; $i < count( $check_subs ); $i++ ) {
             if ( $cart_product_id == $check_subs[ $i ]['product_id'] && $check_subs[ $i ]['status'] == 'active' ) {
+              $this_status = $check_subs[ $i ]['status'];
+
                 wc_add_notice( 
                     'You can\'t subscribe to this product because you already have it and it\'s <strong> 
                     <span style="color: green; text-transform: Capitalize;">
-                    ' . $check_subs[ $i ]['status'] . '. 
+                    '. $this_status.'
                     </span>
                     </strong> Please consider an 
                     <strong><a href="#">
@@ -97,11 +83,13 @@ class LibreSignBlockPurchaseSameSubscriptionAndStatus
                     <strong><a href="#">contact support</a></strong> if you have any questions.', 
                     'error' 
                 );
-                add_filter( 'wc_add_to_cart_message_html', '__return_empty_string', 10, 2 );
+                
+                //remove this product from the cart
                 WC()->cart->remove_cart_item( key( $cart_items ) );
+                // To avoid the message "Product has been added to your cart" after removing the product from the cart.
+                add_filter( 'wc_add_to_cart_message_html', '__return_empty_string', 10, 2 );
             }
         }
     }
-      
 
 }
